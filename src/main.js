e82a192639c667fb37d862876594502cdf99d803
carlos <carlos.alvarez@conveors-immsa.com>
2021-07-27 09:22:14 -0500

    comite

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'; // Importo Vuetify 
-import router from 'vue-router'
  
 Vue.config.productionTip = false
  
 Vue.use(vuetify);
-Vue.use(router);
 
 new Vue({
   vuetify, // Vuetify 

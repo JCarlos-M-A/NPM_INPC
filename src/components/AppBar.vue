da2be409944c2147fcd5880afb6abbce6cb098bc
carlos <carlos.alvarez@conveors-immsa.com>
2021-07-27 08:20:12 -0500

    Cambios

---
+++ src/components/AppBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-toolbar>
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title>Title</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn elevation="0" @click='direccion("/inicio")'>Inicio</v-btn>
+      <v-btn elevation="0" @click='direccion("/nosotros")'>Nosotros</v-btn>
+      <v-btn elevation="0" @click='direccion("/proyectos")'>Proyectos</v-btn>
+      <v-btn elevation="0" @click='direccion("/contacto")'>Contacto</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'AppBar',
+  data: () => ({
+    //
+    
+  }),
+ methods: {
+      direccion(direccion){
+          location.href=direccion;
+      }
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import vuetify from './plugins/vuetify'; // Importo Vuetify 
+import router from './router' // Importo Vue Router 
+ 
 Vue.config.productionTip = false
-
+ 
+Vue.use(vuetify);
+ 
 new Vue({
-  render: h => h(App),
+  vuetify, // Vuetify 
+  router, // Vue Router
+  render: h => h(App)
 }).$mount('#app')
